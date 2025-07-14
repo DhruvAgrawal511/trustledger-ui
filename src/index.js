64d49c11c6a875cd7cc41e35f294d4670073c27f
Dhruv Agrawal <dhruv@dhruv>
2025-07-14 19:05:01 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import AdminLogin from './AdminLogin';
-import AdminDashboard from './AdminDashboard';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import AdminLogin from "./AdminLogin";
+import AdminDashboard from "./AdminDashboard";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/admin" element={<AdminLogin />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/admin" element={<AdminLogin />} />
+      <Route path="/dashboard" element={<AdminDashboard />} />
+    </Routes>
+  </BrowserRouter>
 );
